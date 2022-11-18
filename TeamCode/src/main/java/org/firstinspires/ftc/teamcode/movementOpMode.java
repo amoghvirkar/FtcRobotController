cd21dd5c186c22448f049e545c0285352e7ee0d7
amoghvirkar <117039663+amoghvirkar@users.noreply.github.com>
2022-11-17 22:37:41 -0800

    Added parameters to TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movementOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movementOpMode.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp
+@TeleOp(name="Basic: Omni Linear OpMode", group="Linear Opmode")
 public class movementOpMode extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
