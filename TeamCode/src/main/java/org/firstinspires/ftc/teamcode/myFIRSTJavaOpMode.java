6024db2edfef0a641579fd33e2e2d98612f9ba49
amoghvirkar <117039663+amoghvirkar@users.noreply.github.com>
2022-11-17 19:22:40 -0800

    The changes to the if statements make the code more understandable. Another if statement was added to make the driver hub show when the button is pressed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/myFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/myFIRSTJavaOpMode.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class myFIRSTJavaOpMode extends LinearOpMode {
     private Gyroscope imu;
@@ -31,16 +32,22 @@ public class myFIRSTJavaOpMode extends LinearOpMode {
         while (opModeIsActive()) {
             tgtPower = -this.gamepad1.left_stick_y;
             motorTest.setPower(tgtPower);
-            if(gamepad1.y) {
+            if (gamepad1.y) {
                 servoTest.setPosition(0);
             } else if (gamepad1.x || gamepad1.b) {
                 servoTest.setPosition(0.5);
             } else if (gamepad1.a) {
                 servoTest.setPosition(1);
             }
+            if (!digitalTouch.getState()) {
+                telemetry.addData("Button", "PRESSED");
+            } else {
+                telemetry.addData("Button", "NOT PRESSED");
+            }
             telemetry.addData("Servo Position", servoTest.getPosition());
             telemetry.addData("Target Power", tgtPower);
             telemetry.addData("Motor Power", motorTest.getPower());
+            telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
