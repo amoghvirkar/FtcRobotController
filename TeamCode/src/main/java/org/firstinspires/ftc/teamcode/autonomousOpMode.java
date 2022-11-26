e7bbf5f5c73f588cc5afbf9feb59c9b68547c3e9
amoghvirkar <117039663+amoghvirkar@users.noreply.github.com>
2022-11-26 14:46:46 -0800

    Create autonomousOpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousOpMode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class autonomousOpMode {
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    
+}
