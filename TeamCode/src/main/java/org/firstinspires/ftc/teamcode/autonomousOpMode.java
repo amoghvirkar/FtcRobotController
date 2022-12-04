cf7e865fca3593321832da438392bc9a2092f6c4
amoghvirkar <117039663+amoghvirkar@users.noreply.github.com>
2022-12-04 15:02:21 -0800

    Added autonomous op mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousOpMode.java
@@ -1,15 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous
-public class autonomousOpMode {
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+@Autonomous(name = "Ice Code Autonomous", group = "Linear Opmode")
+public class autonomousOpMode extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
-    
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    public void runOpMode() {
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+        while (opModeIsActive()) {
+            
+        }
+    }
 }
