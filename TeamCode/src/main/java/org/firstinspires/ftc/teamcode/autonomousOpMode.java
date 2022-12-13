2ebc7ee3e29234b38297f9d988aee84158bf3923
amoghvirkar <117039663+amoghvirkar@users.noreply.github.com>
2022-12-12 21:04:03 -0800

    Added methods to the autonomousOpMode to move forward and do a quarter turn.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousOpMode.java
@@ -4,7 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import java.util.concurrent.TimeUnit;
 
 @Autonomous(name = "Ice Code Autonomous", group = "Linear Opmode")
 public class autonomousOpMode extends LinearOpMode{
@@ -13,21 +15,59 @@ public class autonomousOpMode extends LinearOpMode{
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
-    public void runOpMode() {
+    private DcMotor viperPulley = null;
+    private void quarterTurn(int numTurns) throws InterruptedException {
+        int turnCounter = 0;
+        while (turnCounter<numTurns) {
+            leftFrontDrive.setPower(1);
+            leftBackDrive.setPower(1);
+            rightFrontDrive.setPower(-1);
+            rightBackDrive.setPower(-1);
+            TimeUnit.SECONDS.sleep(5);
+            turnCounter++;
+        }
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+    }
+    private void moveForward(int time) throws InterruptedException {
+        leftFrontDrive.setPower(1);
+        leftBackDrive.setPower(1);
+        rightFrontDrive.setPower(1);
+        rightBackDrive.setPower(1);
+        TimeUnit.SECONDS.sleep(time);
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+    }
+    private void pickUpCone(int junction) throws InterruptedException {
+        int junctionCounter = 0;
+    }
+    public void runOpMode() throws InterruptedException {
         leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
         leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        viperPulley = hardwareMap.get(DcMotor.class, "viper_slide_controller");
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        viperPulley.setDirection(DcMotorSimple.Direction.FORWARD);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         waitForStart();
         runtime.reset();
         while (opModeIsActive()) {
-            
+            moveForward(5);
+            quarterTurn(3);
+            //pick up cone
+            quarterTurn(2);
+            moveForward(5);
+            quarterTurn(3);
+            //place cone
         }
     }
 }
